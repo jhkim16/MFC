96fe1ddf6ed20a1357bbec47c7dc7784bf5726a4
jhkim16 <kjh960916@naver.com>
2019-10-01 11:07:21 +0900

    work

---
+++ imgTest/1001imgTestDlg.h
@@ -0,0 +1,37 @@
+﻿
+// 1001imgTestDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CMy1001imgTestDlg 대화 상자
+class CMy1001imgTestDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CMy1001imgTestDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+	CImage img;
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MY1001IMGTEST_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+};
